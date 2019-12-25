48f2e7bb8c0090a9de57de5cce2234f4fe7101de
kwccoin <kwccoin@yahoo.com>
2019-12-25 15:50:09 +0000

    seems not working totally see run-error-log.txt

---
+++ mytestleinproj/clojure-noob/src/clojure_noob/core.clj
@@ -0,0 +1,29 @@
+; jdk and clojure
+; ===============
+; brew cask install adoptopenjdk
+; brew install clojure 
+
+; lein
+; ====
+; https://www.hildeberto.com/2015/07/installing-leiningen-on-mac-os.html
+; but need to run 
+; export PATH=$PATH:/Applications/clojure
+; as ~/.bash_profle not work, is it the mac z shell thing???
+
+; https://www.braveclojure.com/getting-started/
+
+; you have to be in a directory with project.clj
+
+; trying the real one 
+; https://github.com/kwccoin/bounty
+; got error after saying 4x4
+
+
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!")
+  (println "I am a teapot"))
