c130b45560a35c12ac448cc2f0e5bbb159dc2348
kwccoin <kwccoin@yahoo.com>
2019-12-28 16:50:29 +0000

    stage g find some bugs and need full state plus the equal card play

--- myJS/cardC-FP-1.js
+++ myJS/cardC-FP-1.js
@@ -6,6 +6,7 @@
 // - stage d : (extract winMessage) try to avoid logic in display message
 // - stage e : (combine end message) 
 // - stage f : (combine sate vars into one)
+// - stage g : (creat a nextState()) basically it is generalisd to nextState( current state) 
 
 // adopt the competition card is discarded as in wiki: https://en.wikipedia.org/wiki/Goofspiel
 
@@ -14,34 +15,53 @@ function runGame() {
                   bountyCards : [1, 2, 3, 4, 5, 6, 7, 8],
                   playerCards : [[1, 2, 3, 4, 5, 6, 7, 8],
                                  [1, 2, 3, 4, 5, 6, 7, 8]],
+                  lastBountyCard: 0,  
+                  lastPlayingCards : [0, 0], // need this to avoid cannot logging in stage g
                   playerScores : [0, 0]} // stage f and many below to use state. !!!
 
     while (state.bountyCards.length > 0) {
         
-        const bountyCard = selectRandom(state.bountyCards);  // stage b - popRandom(bountyCards); 
-        state.bountyCards = without(state.bountyCards, bountyCard) // stage b - 
+        console.log(`-------------------------------\n`+
+                    `Before Turn: ${state.turn}:`+ 
+                    `\n\tbountyCards: ${state.bountyCards}, ` +
+                    `\n\tplayerCards: ${state.playerCards}, ` +
+                    `\n\tlastBountyCards: ${state.lastBountyCard}, ` +
+                    `\n\tlastPlayingCards: ${state.lastPlayingCards}, ` +
+                    `\n\tplayerScores: ${state.playerScores}, `);
         
-        console.log(`Turn ${state.turn}: Bounty: ${bountyCard}`);
-        const card0 = playRandomStrategy(state.playerCards[0], bountyCard);
-        const card1 = playEqualStrategy(state.playerCards[1], bountyCard);
-        state.playerCards[0] = without(state.playerCards[0], card0); // stage b
-        state.playerCards[1] = without(state.playerCards[1], card1); // stage c
+        state = nextState(state);
         
-        state.turn +=1;
+        // stage g - const bountyCard = selectRandom(state.bountyCards);  // stage b - popRandom(bountyCards); 
+        // stage g - state.bountyCards = without(state.bountyCards, bountyCard) // stage b - 
         
-        console.log(`\tPlayer 0 plays: ${card0}`); // stage - a 
-        console.log(`\tPlayer 1 plays: ${card1}`); // stage - a 
+        console.log(`After Turn: ${state.turn}:`+ 
+                    `\n\tbountyCards: ${state.bountyCards}, ` +
+                    `\n\tplayerCards: ${state.playerCards}, ` +
+                    `\n\tlastBountyCards: ${state.lastBountyCard}, ` +
+                    `\n\tlastPlayingCards: ${state.lastPlayingCards}, ` +
+                    `\n\tplayerScores: ${state.playerScores}, `+
+                    `\n======================================`);
+
+        // stage g - const card0 = playRandomStrategy(state.playerCards[0], bountyCard);
+        // stage g - const card1 = playEqualStrategy(state.playerCards[1], bountyCard);
+        // stage g - state.playerCards[0] = without(state.playerCards[0], card0); // stage b
+        // stage g - state.playerCards[1] = without(state.playerCards[1], card1); // stage c
+        
+        // stage g - state.turn +=1;
+        
+        console.log(`\tPlayer 0 plays: ${state.lastPlayingCards[0]}`)// card0}`); // stage - a 
+        console.log(`\tPlayer 1 plays: ${state.lastPlayingCards[1]}`)// card1}`); // stage - a 
                                                    // ; see wiki best strategy against random play
         
-        if (card0 > card1) {
-            state.playerScores[0] += bountyCard;
-        } else if (card1 > card0){
-            state.playerScores[1] += bountyCard;
-        } else {
-            //console.log(`\ncard0 is ${card0}`);
-            //console.log(`card1 is ${card1}`);
-            console.log("the competition card is discarded")
-        }
+        // stage g - if (card0 > card1) {
+        // stage g -     state.playerScores[0] += bountyCard;
+        // stage g - } else if (card1 > card0){
+        // stage g -     state.playerScores[1] += bountyCard;
+        // stage g - } else {
+        // stage g -     //console.log(`\ncard0 is ${card0}`);
+        // stage g -     //console.log(`card1 is ${card1}`);
+        // stage g -    console.log("the competition card is discarded")
+        // stage g - }
     }
     
     // stage e - console.log(`Scores: ${playerScores[0]} v ${playerScores[1]}`);
@@ -61,6 +81,57 @@ function runGame() {
 
 runGame();
 
+function nextState(state) {
+
+        const bountyCard = selectRandom(state.bountyCards);  // stage b - popRandom(bountyCards); 
+        // no need just return : state.bountyCards = without(state.bountyCards, bountyCard) // stage b - 
+        
+        // remain in the main logic : console.log(`Turn ${state.turn}: Bounty: ${bountyCard}`);
+        const card0 = playRandomStrategy(state.playerCards[0], bountyCard);
+        const card1 = playEqualStrategy(state.playerCards[1], bountyCard);
+        // no need just return : state.playerCards[0] = without(state.playerCards[0], card0); // stage b
+        // no need just return : state.playerCards[1] = without(state.playerCards[1], card1); // stage c
+        
+        // no need just return : state.turn +=1;
+        
+        // remain in the main logic : console.log(`\tPlayer 0 plays: ${card0}`); // stage - a 
+        // remain in the main logic : console.log(`\tPlayer 1 plays: ${card1}`); // stage - a 
+                                                   // ; see wiki best strategy against random play
+        
+        // use a new function - if (card0 > card1) {
+        // use a new function -     state.playerScores[0] += bountyCard;
+        // use a new function - } else if (card1 > card0){
+        // use a new function -     state.playerScores[1] += bountyCard;
+        // use a new function - } else {
+        // use a new function -     //console.log(`\ncard0 is ${card0}`);
+        // use a new function -     //console.log(`card1 is ${card1}`);
+        // use a new function -     console.log("the competition card is discarded")
+        // use a new function - }
+    
+        return {turn: state.turn + 1,
+                bountyCards: without(state.bountyCards, bountyCard),
+                playerCards: [without(state.playerCards[0], card0),  // source has a bugs without card0
+                              without(state.playerCards[1], card1)], // source has a bugs without card1
+                lastBountyCard: bountyCard, 
+                lastPlayingCards : [card0, card1],
+                playerScores: newScore(state.playerScores, 
+                                       [card0, card1],
+                                       bountyCard) // source has a bugs without bountyCard1
+        }
+}
+
+function newScore(playerScores, playerCards, bountyCard){
+    if (playerCards[0] > playerCards[1]) {
+            return [playerScores[0]+bountyCard, playerScores[1]]; // source has a bugs not + 1
+        } else if (playerCards[0] < playerCards[1]){
+            return [playerScores[0]  ,  playerScores[1]+bountyCard]; // source has a bugs not + 
+        } else {
+          // no need  //console.log(`\ncard0 is ${card0}`);
+          // no need  //console.log(`card1 is ${card1}`);
+          // no need  console.log("the competition card is discarded")
+            return [playerScores[0]  ,  playerScores[1]]  // actually need this as still need to return
+          }
+}
 
 function scoreMessage(playerScores){
     return `Scores: ${playerScores[0]} v ${playerScores[1]}`;
