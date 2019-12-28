ba6fcf63904f519bede7021f0bbdbeb358f539d3
kwccoin <kwccoin@yahoo.com>
2019-12-28 22:07:00 +0000

    align the fun with clojure

--- myJS/cardC-FP-1.js
+++ myJS/cardC-FP-1.js
@@ -14,41 +14,31 @@
 
 // the competition card is discarded as in wiki: https://en.wikipedia.org/wiki/Goofspiel
 
-function runGame() {
-    let states = [{ turn : 0,
-                  bountyCards : [1, 2, 3, 4, 5, 6, 7, 8],
-                  playerCards : [[1, 2, 3, 4, 5, 6, 7, 8],
-                                 [1, 2, 3, 4, 5, 6, 7, 8]],
-                  lastBountyCard: 0,         // need this to help logging 
-                  lastPlayingCards : [0, 0], // need this to help logging 
-                  playerScores : [0, 0]}] 
-
-    return recur(states, nextState, function(state){
-                 return state.bountyCards.length == 0; // not > 0 
-                 });
-    
+function without(arr, value){                                
+    const index = arr.indexOf(value);                        
+    return [...arr.slice(0, index), ...arr.slice(index+1)];  
 }
 
-
-function recur(states, stateChange, endCondition){
-    if(endCondition(last(states))){ // missing one ) in the code
-        return states;
+function winMessage(playerScores) {
+    if (playerScores[0] == playerScores[1]) {
+        return "PLayer Tie.\n";
+    } else if (playerScores[0] > playerScores[1]) {
+        return "PLayer 0 Wins!\n";
     } else {
-        return recur(states.concat(stateChange(last(states))),stateChange, endCondition); // lastState should be last(states) and the function recur need 3 parameters not 1 parameter
+        return "PLayer 1 Wins!\n";
     }
 }
 
-function report(states, onTurn, onEnd){
-    return states.map(onTurn).join("\n")+"\n"+
-        onEnd(last(states).playerScores) // last(states) not lastState
+
+function scoreMessage(playerScores){
+    return `Scores: ${playerScores[0]} v ${playerScores[1]}`;
 }
 
-console.log(report(runGame(), turnMessage, endMessage));
-
-
-function last(arr){
-    return arr[arr.length - 1];
+function endMessage(playerScores){
+    return scoreMessage(playerScores) + "\n" + 
+           winMessage(playerScores);
 }
+
 
 function turnMessage(state){ 
        return   `--------------------------------\n`+
@@ -59,6 +49,23 @@ function turnMessage(state){
             `\n\t  current player Scores  : ${state.playerScores[0]} and ${state.playerScores[1]}, ` +
             `\n\t  remaining bounty Cards : ${state.bountyCards}, ` +
             `\n\t  on hand player Cards   : ${state.playerCards[0]} and ${state.playerCards[1]}. ` 
+}
+
+
+function report(states, onTurn, onEnd){
+    return states.map(onTurn).join("\n")+"\n"+
+        onEnd(last(states).playerScores) // last(states) not lastState
+}
+
+
+function newScore(playerScores, playerCards, bountyCard){
+    if (playerCards[0] > playerCards[1]) {
+            return [playerScores[0]+bountyCard, playerScores[1]]; //  not + 1
+        } else if (playerCards[0] < playerCards[1]){
+            return [playerScores[0]  ,  playerScores[1]+bountyCard]; //  not + 1
+        } else {
+            return [playerScores[0]  ,  playerScores[1]]  // need this to return otherwise no state when card is equal (not needed under OO and imperative)
+          }
 }
 
 function nextState(state) {
@@ -80,34 +87,40 @@ function nextState(state) {
         }
 }
 
-function newScore(playerScores, playerCards, bountyCard){
-    if (playerCards[0] > playerCards[1]) {
-            return [playerScores[0]+bountyCard, playerScores[1]]; //  not + 1
-        } else if (playerCards[0] < playerCards[1]){
-            return [playerScores[0]  ,  playerScores[1]+bountyCard]; //  not + 1
-        } else {
-            return [playerScores[0]  ,  playerScores[1]]  // need this to return otherwise no state when card is equal (not needed under OO and imperative)
-          }
-}
 
-function scoreMessage(playerScores){
-    return `Scores: ${playerScores[0]} v ${playerScores[1]}`;
-}
-
-function endMessage(playerScores){
-    return scoreMessage(playerScores) + "\n" + 
-           winMessage(playerScores);
-}
-
-function winMessage(playerScores) {
-    if (playerScores[0] == playerScores[1]) {
-        return "PLayer Tie.\n";
-    } else if (playerScores[0] > playerScores[1]) {
-        return "PLayer 0 Wins!\n";
+function recur(states, stateChange, endCondition){
+    if(endCondition(last(states))){ // missing one ) in the code
+        return states;
     } else {
-        return "PLayer 1 Wins!\n";
+        return recur(states.concat(stateChange(last(states))),stateChange, endCondition); // lastState should be last(states) and the function recur need 3 parameters not 1 parameter
     }
 }
+
+
+function runGame() {
+    let states = [{ turn : 0,
+                  bountyCards : [1, 2, 3, 4, 5, 6, 7, 8],
+                  playerCards : [[1, 2, 3, 4, 5, 6, 7, 8],
+                                 [1, 2, 3, 4, 5, 6, 7, 8]],
+                  lastBountyCard: 0,         // need this to help logging 
+                  lastPlayingCards : [0, 0], // need this to help logging 
+                  playerScores : [0, 0]}] 
+
+    return recur(states, nextState, function(state){
+                 return state.bountyCards.length == 0; // not > 0 
+                 });
+    
+}
+
+
+
+console.log(report(runGame(), turnMessage, endMessage));
+
+
+function last(arr){
+    return arr[arr.length - 1];
+}
+
 
 function selectRandom(arr){ 
     const index = Math.floor(Math.random() * arr.length);
@@ -123,9 +136,5 @@ function playEqualStrategy(playerCards, bountyCard){
     return bountyCard;
 }
 
-function without(arr, value){                                
-    const index = arr.indexOf(value);                        
-    return [...arr.slice(0, index), ...arr.slice(index+1)];  
-}
 
 module.exports = runGame; // for running under external call
